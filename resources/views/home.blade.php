91e381cabd7b74b7bbb80155e9b14a46d0d6555e
lauricdd <lauri_cdd@hotmail.com>
2017-07-13 19:28:42 -0500

    Fixed map and scroll form to have 100% height

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,170 +16,174 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4 scrollcol">
-                    <form method="POST" action="{{ url('/') }}">
-                        {{csrf_field()}}
+                    <div id="form">
+                        <form method="POST" action="{{ url('/') }}">
+                            {{csrf_field()}}
 
-                        <div class="row row-margin">
-                            <div class="col-xs-12">
-                                <h3><strong>@lang('Available Data')</strong></h3>
-                                <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+                            <div class="row row-margin">
+                                <div class="col-xs-12">
+                                    <h3><strong>@lang('Available Data')</strong></h3>
+                                    <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+                                </div>
+
+                                <div class="col-md-8 col-xs-8 col-sm-8 pull-right">
+                                    <div class="col-md-4 col-sm-4 col-xs-4">
+                                        <small>@lang('Real Time')</small>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-4">
+                                        <small>@lang('Non RT')</small>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-4">
+                                        <small>@lang('Off')</small>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="col-md-8 col-xs-8 col-sm-8 pull-right">
+                            <div class="row row-margin">
                                 <div class="col-md-4 col-sm-4 col-xs-4">
-                                    <small>@lang('Real Time')</small>
+                                    <div class="radio">
+                                        <label class="small-label"><input type="radio" name="station_type" value="water_quality" checked> @lang('Water Quality')</label>
+                                    </div>
                                 </div>
+
+                                <div class="col-md-8 col-xs-8 col-sm-8">
+                                    <div class="col-md-4 col-sm-4 col-xs-4">
+                                        <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-4">
+                                        <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-4">
+                                        <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row row-margin">
                                 <div class="col-md-4 col-sm-4 col-xs-4">
-                                    <small>@lang('Non RT')</small>
+                                    <div class="radio">
+                                        <label class="small-label"><input type="radio" name="station_type" value="hydrometereologic_factors" >@lang('Hydro-Meteorologic Factors')</label>
+                                    </div>
                                 </div>
-                                <div class="col-md-4 col-sm-4 col-xs-4">
-                                    <small>@lang('Off')</small>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="row row-margin">
-                            <div class="col-md-4 col-sm-4 col-xs-4">
-                                <div class="radio">
-                                    <label class="small-label"><input type="radio" name="station_type" value="water_quality" checked> @lang('Water Quality')</label>
-                                </div>
-                            </div>
-
-                            <div class="col-md-8 col-xs-8 col-sm-8">
-                                <div class="col-md-4 col-sm-4 col-xs-4">
-                                    <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
-                                </div>
-                                <div class="col-md-4 col-sm-4 col-xs-4">
-                                    <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
-                                </div>
-                                <div class="col-md-4 col-sm-4 col-xs-4">
-                                    <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row row-margin">
-                            <div class="col-md-4 col-sm-4 col-xs-4">
-                                <div class="radio">
-                                    <label class="small-label"><input type="radio" name="station_type" value="hydrometereologic_factors" >@lang('Hydro-Meteorologic Factors')</label>
+                                <div class="col-md-8 col-xs-8 col-sm-8">
+                                    <div class="col-md-4 col-sm-4 col-xs-4">
+                                        <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-4">
+                                        <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-4">
+                                        <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
+                                    </div>
                                 </div>
                             </div>
 
-                            <div class="col-md-8 col-xs-8 col-sm-8">
-                                <div class="col-md-4 col-sm-4 col-xs-4">
-                                    <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
-                                </div>
-                                <div class="col-md-4 col-sm-4 col-xs-4">
-                                    <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
-                                </div>
-                                <div class="col-md-4 col-sm-4 col-xs-4">
-                                    <img src="/images/marker-icon-2x.png" class="marker" alt="marker">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row row-margin">
-                            <div class="form-group">
-                                <label class="control-label col-md-3 col-sm-3 col-xs-4" for="from"> @lang('Start Date') </label>
-                                <div class="col-md-8 col-sm-6 col-xs-8">
-                                    <input id="from" type="text" class="form-control datepicker" name="start_date">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row row-margin">
-                            <div class="form-group">
-                                <label class="control-label col-md-3 col-sm-3 col-xs-4" for="to"> @lang('End Date') </label>
-                                <div class="col-md-8 col-sm-6 col-xs-8">
-                                    <input id="to" type="text" class="form-control datepicker" name="end_date">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row row-margin">
-                            <label class="control-label col-md-3 col-sm-3 col-xs-4"> @lang('Preselect End Date') </label>
-                            <div class="col-md-8 col-sm-6 col-xs-8">
-                                <ul class="inline-links">
-                                    <li><a href="javascript:void(0)"> @lang('1 Day') </a></li>
-                                    <li><a href="javascript:void(0)"> @lang('1 Week') </a></li>
-                                    <li><a href="javascript:void(0)"> @lang('1 Month') </a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="row row-margin">
-                            <div class="form-group">
-                                <label class="control-label col-md-3 col-sm-3 col-xs-4" for="station"> @lang('Station') </label>
-                                <div class="col-md-8 col-sm-6 col-xs-8">
-                                    <select class="form-control" id="station" name="station">
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row row-margin">
-                            <div class="form-group">
-                                <label class="control-label col-md-3 col-sm-3 col-xs-4" for="parameter"> @lang('Parameter') </label>
-                                <div class="col-md-8 col-sm-6 col-xs-8">
-                                    <select class="form-control" id="parameter" name="parameter">
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="form-group">
-                                <label class="control-label col-md-3 col-sm-3 col-xs-4" for="parameter"> @lang('Output Format') </label>
-                                <div class="col-md-8 col-sm-6 col-xs-8">
-                                    <label class="radio-inline">
-                                        <input type="radio" name="output_format" value="graph" checked> @lang('Graph')
-                                    </label>
-                                    <label class="radio-inline">
-                                        <input type="radio" name="output_format" value="csv"> @lang('CSV File')
-                                    </label>
-                                </div>
-
-                            </div>
-                        </div>
-
-                        <br>
-
-                        <div class="row row-margin">
-                            <div class="col-md-12">
+                            <div class="row row-margin">
                                 <div class="form-group">
-                                    <button type="submit" class="btn btn-primary btn-lg btn-block"> @lang('Get Data') </button>
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-4" for="from"> @lang('Start Date') </label>
+                                    <div class="col-md-8 col-sm-6 col-xs-8">
+                                        <input id="from" type="text" class="form-control datepicker" name="start_date">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="row row-margin">
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-4" for="to"> @lang('End Date') </label>
+                                    <div class="col-md-8 col-sm-6 col-xs-8">
+                                        <input id="to" type="text" class="form-control datepicker" name="end_date">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row row-margin">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-4"> @lang('Preselect End Date') </label>
+                                <div class="col-md-8 col-sm-6 col-xs-8">
+                                    <ul class="inline-links">
+                                        <li><a href="javascript:void(0)"> @lang('1 Day') </a></li>
+                                        <li><a href="javascript:void(0)"> @lang('1 Week') </a></li>
+                                        <li><a href="javascript:void(0)"> @lang('1 Month') </a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="row row-margin">
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-4" for="station"> @lang('Station') </label>
+                                    <div class="col-md-8 col-sm-6 col-xs-8">
+                                        <select class="form-control" id="station" name="station">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row row-margin">
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-4" for="parameter"> @lang('Parameter') </label>
+                                    <div class="col-md-8 col-sm-6 col-xs-8">
+                                        <select class="form-control" id="parameter" name="parameter">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-group">
+                                    <label class="control-label col-md-3 col-sm-3 col-xs-4" for="parameter"> @lang('Output Format') </label>
+                                    <div class="col-md-8 col-sm-6 col-xs-8">
+                                        <label class="radio-inline">
+                                            <input type="radio" name="output_format" value="graph" checked> @lang('Graph')
+                                        </label>
+                                        <label class="radio-inline">
+                                            <input type="radio" name="output_format" value="csv"> @lang('CSV File')
+                                        </label>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <br>
+
+                            <div class="row row-margin">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary btn-lg btn-block"> @lang('Get Data') </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
                 <div class="col-md-8 fixedcol">
-                    <div id="map"></div>
-
-                    <div class="row">
-                        <div class="col-md-2">
-                            <small><strong> @lang('Water Quality labels') </strong></small>
-                        </div>
-
-                        <div class="col-md-10">
-                            <a href="#"><span class="label label-success"> @lang('Optimal') </span></a>
-                            <a href="#"><span class="label label-info"> @lang('Good') </span></a>
-                            <a href="#"><span class="label label-warning"> @lang('Average') </span></a>
-                            <a href="#"><span class="label label-danger"> @lang('Poor') </span></a>
-                        </div>
+                    <div id="map">
+                        <div id="leafletMap"></div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    {{--<div class="row">--}}
+        {{--<div class="col-md-2">--}}
+            {{--<small><strong> @lang('Water Quality labels') </strong></small>--}}
+        {{--</div>--}}
+
+        {{--<div class="col-md-10">--}}
+            {{--<a href="#"><span class="label label-success"> @lang('Optimal') </span></a>--}}
+            {{--<a href="#"><span class="label label-info"> @lang('Good') </span></a>--}}
+            {{--<a href="#"><span class="label label-warning"> @lang('Average') </span></a>--}}
+            {{--<a href="#"><span class="label label-danger"> @lang('Poor') </span></a>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
 @endsection
 
@@ -224,7 +228,7 @@
             }
 
             <!-- Leaflet Map -->
-            var map = L.map('map').setView([10.4207375,-75.5475544], 15);
+            var map = L.map('leafletMap').setView([10.4207375,-75.5475544], 15);
 
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
                 maxZoom: 18,
