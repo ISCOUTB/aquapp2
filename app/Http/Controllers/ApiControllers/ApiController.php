1a2305a4df44517e29f9338f7a175d978e6c65dc
lauricdd <lauri_cdd@hotmail.com>
2017-08-29 15:35:13 -0500

    Create trait to handle api responses and ApiController to use it. Create api NodeController and customize index and show methods. Add route to api.php

---
+++ app/Http/Controllers/ApiControllers/ApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ApiControllers;
+
+use App\Traits\ApiResponser;
+use App\Http\Controllers\Controller;
+
+class ApiController extends Controller
+{
+    use ApiResponser;
+}
