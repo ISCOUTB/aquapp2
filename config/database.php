f6e278c059919b4511adac4fb1a82f25c79d53e0
Jairo Serrano <jairo.serrano@gmail.com>
2017-10-02 21:58:17 -0500

    Updates

--- config/database.php
+++ config/database.php
@@ -90,6 +90,19 @@ return [
             ]
         ],
 
+        'redis' => [
+
+            'client' => 'predis',
+
+            'default' => [
+                'host' => env('REDIS_HOST', 'localhost'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => 0,
+            ],
+
+        ],
+
     ],
 
     /*
