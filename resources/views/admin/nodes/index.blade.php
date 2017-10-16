6d841053ad96d8a9cd26c444f313b9508c9c9557
lauricdd <lauri_cdd@hotmail.com>
2017-10-16 08:20:13 -0500

    Admin index tables responsiveness (footable)

--- resources/views/admin/nodes/index.blade.php
+++ resources/views/admin/nodes/index.blade.php
@@ -6,11 +6,11 @@
 
 @section('admin-content')
     <a class="btn btn-primary pull-right" href="{{ url('admin/nodes/create') }}"> <i class="fa fa-plus"></i> @lang('Add new node') </a>
-    <br>
+    <br><br>
 
     <p class="title">
-        <h2 style="margin-bottom: 0;">@lang('Nodes')</h2>
-        <small class="text-primary">@lang('Dashboard') > @lang('Nodes')</small>
+    <h2 style="margin-bottom: 0;">@lang('Nodes')</h2>
+    <small class="text-primary">@lang('Dashboard') > @lang('Nodes')</small>
     </p>
 
     @if (session('success-update'))
@@ -22,46 +22,45 @@
 
     <table class="table" id="table" data-show-toggle="true" data-paging="true" data-sorting="true" data-filtering="true"> <!--data-expand-first="true"-->
         <thead>
-            <tr>
-                <th>@lang('Name')</th>
-                <th>@lang('Location')</th>
-                <th data-breakpoints="xs">@lang('Coordinates')</th>
-                <th data-breakpoints="xs sm">@lang('Status')</th>
-                <th data-breakpoints="xs sm"> @lang('Actions')</th>
-                <th data-breakpoints="all">@lang('Node Type')</th>
-            </tr>
+        <tr>
+            <th data-breakpoints="xs sm"></th>
+            <th>@lang('Name')</th>
+            <th data-breakpoints="xs">@lang('Location')</th>
+            <th data-breakpoints="xs sm md">@lang('Coordinates')</th>
+            <th data-breakpoints="all">@lang('Status')</th>
+            <th data-breakpoints="all">@lang('Node Type')</th>
+        </tr>
         </thead>
         <tbody>
-            @foreach($nodes as $node)
-                <tr>
-                    <td>{{ $node->name }}</td>
-                    <td>{{ $node->location }}</td>
-                    <td>{{ $node->coordinates[0] }}, {{ $node->coordinates[1] }}</td>
-                    <td>
-                        @if($node->status == 'Real Time')
-                            @lang('Real Time')
-                        @elseif( $node->status == 'Non RT')
-                            @lang('Non RT')
-                        @else
-                            @lang('Off')
+        @foreach($nodes as $node)
+            <tr>
+                <td><a class="btn-link fa fa-pencil" href="{{route('nodes.edit', $node->id)}}"></a></td>
+                <td>{{ $node->name }}</td>
+                <td>{{ $node->location }}</td>
+                <td>{{ $node->coordinates[0] }}, {{ $node->coordinates[1] }}</td>
+                <td>
+                    @if($node->status == 'Real Time')
+                        @lang('Real Time')
+                    @elseif( $node->status == 'Non RT')
+                        @lang('Non RT')
+                    @else
+                        @lang('Off')
+                    @endif
+                </td>
+                <td><strong class="text-primary">@lang('Name') => </strong> {{ $node->node_type->name }}.
+                    <br>
+                    <strong class="text-primary">@lang('Data Delimiter') =></strong> {{ $node->separator }}.
+                    <br>
+                    <strong class="text-primary">@lang('Sending Schema') =></strong>
+                    @foreach($node->node_type->sensors as $sensor)
+                        <small>{{ $sensor["variable"] }}</small>
+                        @if(!$loop->last)
+                            ,
                         @endif
-                    </td>
-                    <td><a class="btn-link fa fa-pencil" href="{{route('nodes.edit', $node->id)}}"></a></td>
-                    <td><strong class="text-primary">@lang('Name') => </strong> {{ $node->node_type->name }}.
-                        <br>
-                        <strong class="text-primary">@lang('Data Delimiter') =></strong> {{ $node->separator }}.
-                        <br>
-                        <strong class="text-primary">@lang('Sending Schema') =></strong>
-                        @foreach($node->node_type->sensors as $sensor)
-                            <small>{{ $sensor["variable"] }}</small>
-                            @if(!$loop->last)
-                                ,
-                            @endif
-                        @endforeach
-                    </td>
-
-                </tr>
-            @endforeach
+                    @endforeach
+                </td>
+            </tr>
+        @endforeach
         </tbody>
     </table>
 @endsection
