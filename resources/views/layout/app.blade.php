6078daed694ca7e96e475536a8e4d52fc447b738
lauricdd <lauri_cdd@hotmail.com>
2017-10-22 21:29:02 -0500

    Home link on navbar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -63,6 +63,7 @@
                     </li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{ url('/') }}"><i class="fa fa-home" aria-hidden="true"></i> &nbsp; @lang('Home') </a></li>
                     <li><a href="{{ url('/admin/nodes') }}"><i class="fa fa-sign-in" aria-hidden="true"></i> &nbsp; @lang('Login') </a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
