1d9e98deebed7858891a69df318a5050e1849b9d
lauricdd <lauri_cdd@hotmail.com>
2017-11-02 12:07:35 -0500

    Refactor date format on SensorData seeder

--- database/seeds/SensorDataTableSeeder.php
+++ database/seeds/SensorDataTableSeeder.php
@@ -6,6 +6,7 @@ use App\Node;
 use App\NodeType;
 use App\SensorData;
 
+
 class SensorDataTableSeeder extends Seeder
 {
     /**
@@ -21,8 +22,10 @@ class SensorDataTableSeeder extends Seeder
 
         $files = File::allFiles($directory);
 
-        if($files) {
-            foreach ($files as $file) {
+        if($files)
+        {
+            foreach ($files as $file)
+            {
                 $fileInfo = pathinfo($file);
                 $nodeId = $fileInfo["filename"];
 
@@ -40,8 +43,7 @@ class SensorDataTableSeeder extends Seeder
 
                         foreach ($lines as $lineNum => $line) {
                             $line = explode($nodeType->separator, $line);
-                            $timestamp = $line[0]; // 11/28/16 08:50:00 PM mm/dd/aa
-                            $timestamp = date("YmdHis", strtotime($timestamp));
+                            $timestamp = $line[0];
 
                             $obj = array(
                                 "timestamp" => $timestamp,
@@ -56,9 +58,7 @@ class SensorDataTableSeeder extends Seeder
                             'node_id' => $node->id,
                             'data' => $data
                         ]);
-
                     }
-
                 }
                 else
                 {
@@ -67,58 +67,22 @@ class SensorDataTableSeeder extends Seeder
 
             }
         }
-
-        /*
-         * Hydro-Meteorologic Factors
-         */
-//        $directory = "database/data/nodes-data/HMF";
-//
-//        $files = File::allFiles($directory);
-//
-//        if($files) {
-//            foreach ($files as $file) {
-//                $fileInfo = pathinfo($file);
-//                $split = explode('-', $fileInfo["filename"]); //59b75c5f9a8920223f2eabe7-Water Temperature
-//                $nodeId = $split[0];
-//                $variableName = $split[1];
-//
-//                //Check if node and variable are valid
-//                $node = Node::find($nodeId);
-//                $variable = NodeType::where('sensors.variable', $variableName)->first();
-//
-//                if($node and $variable) {
-//                    $lines = file($fileInfo["dirname"]. '/' . $fileInfo["basename"], FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-//
-//                    echo $fileInfo["basename"] . "\n";
-//
-//                    $data = [];
-//                    foreach ($lines as $lineNum => $line) {
-//                        $line = explode(',', $line); // explode by separator
-//                        $timestamp = $line[0]; // 11/28/16 08:50:00 PM
-//                        $timestamp = date("YmdHis", strtotime($timestamp));
-//
-//                        $obj = array(
-//                            "timestamp" => $timestamp,
-//                            "value" => (float) $line[1]
-//                        );
-//
-//                        $data[] = $obj;
-//                    }
-//
-//                    SensorData::create([
-//                        'variable' => $variableName,
-//                        'node_id' => $node->id,
-//                        'data' => $data
-//                    ]);
-//
-//                }
-//                else
-//                {
-//                    continue;
-//                }
-//
-//            }
-//        }
-
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
