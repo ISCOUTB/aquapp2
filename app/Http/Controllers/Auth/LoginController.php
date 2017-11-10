e210a06478f0b4ee95c7753a744f2a844f4f03f8
lauricdd <lauri_cdd@hotmail.com>
2017-11-10 18:03:48 -0500

    Redirect path in LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\NodeType;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
