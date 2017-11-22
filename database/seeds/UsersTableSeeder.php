4c41873caaf1aa1a400f239ea60c3736c9c09e95
lauricdd <lauri_cdd@hotmail.com>
2017-11-21 21:28:16 -0500

    Reset users on seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,6 +13,8 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->delete();
+
         User::create([
             'name'     => 'Admin',
             'email' => env('ADMIN_EMAIL'),
